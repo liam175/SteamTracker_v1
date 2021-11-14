c8b1e80523fd020bc626166188a29d4b1e0e69bc
liam175 <lostlakeboy@gmail.com>
2021-11-13 22:28:38 -0600

    serial update, failed, saved for future use (possibly)

--- app/src/main/java/com/example/steamcounter2020v1_0/MainActivity.kt
+++ app/src/main/java/com/example/steamcounter2020v1_0/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.steamcounter2020v1_0
 
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbDeviceConnection
+import android.hardware.usb.UsbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.CheckBox
 import android.widget.SeekBar
 import android.widget.Switch
 import android.widget.TextView
+import com.felhr.usbserial.UsbSerialDevice
+import com.felhr.usbserial.UsbSerialInterface
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
@@ -21,11 +32,100 @@ class MainActivity : AppCompatActivity() {
 
     var inTele = false
 
+    var dataSave : List<String>? = null
+
+    lateinit var  m_usbManager: UsbManager
+    var m_device: UsbDevice? = null
+    var m_serial: UsbSerialDevice? = null
+    var m_connection: UsbDeviceConnection? = null
+
+    val ACTION_USB_PERMISSION = "permission"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        m_usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+
+        var filter = IntentFilter()
+        filter.addAction(ACTION_USB_PERMISSION)
+        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_ATTACHED)
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+        registerReceiver(brodcastReciver, filter)
     }
 
+    fun startUsb(){
+        val usbDevices: HashMap<String, UsbDevice>? = m_usbManager.deviceList
+        if(!usbDevices?.isEmpty()!!){
+            var keep = true
+            usbDevices.forEach{ entry ->
+                m_device = entry.value
+                val deviceVendorId: Int? = m_device?.vendorId
+                Log.i("serial","vendor id "+deviceVendorId)
+                if(deviceVendorId == 1027){
+                    val intent: PendingIntent = PendingIntent.getBroadcast(this,0, Intent(ACTION_USB_PERMISSION), 0)
+                    m_usbManager.requestPermission(m_device, intent)
+                    keep = false
+                    Log.i("serial","successful connection")
+                }else{
+                    m_connection = null
+                    m_device = null
+                    Log.i("serial","unable to connect")
+                }
+                if(!keep){
+                    return
+                }
+            }
+        }else{
+            Log.i("serial","no usb devices connected " + usbDevices)
+        }
+    }
+
+    private fun sendData(input: String){
+        //duh
+        m_serial?.write(input.toByteArray())
+        Log.i("serial", "sending data: "+input.toByteArray())
+    }
+
+    public fun disconnect(){
+        //it cant be this easy
+        m_serial?.close()
+    }
+
+    private val brodcastReciver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            //does it have perms
+            if(intent?.action!! == ACTION_USB_PERMISSION){
+                val granted: Boolean = intent.extras!!.getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED)
+                //iff yes set up connections
+                if(granted){
+                    m_connection = m_usbManager.openDevice(m_device)
+                    m_serial = UsbSerialDevice.createUsbSerialDevice(m_device,m_connection)
+                    if(m_serial!=null){
+                        if(m_serial!!.open()){
+                            m_serial!!.setBaudRate(9600)
+                            m_serial!!.setDataBits(UsbSerialInterface.DATA_BITS_8)
+                            m_serial!!.setStopBits(UsbSerialInterface.STOP_BITS_1)
+                            m_serial!!.setParity(UsbSerialInterface.PARITY_NONE)
+                            m_serial!!.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF)
+                        }else{
+                            Log.i("serial","port not open")
+                        }
+                    }else{
+                        Log.i("serial","port is null")
+                    }
+                }else{
+                    Log.i("serial","permission failed")
+                }
+            }else if(intent.action == UsbManager.ACTION_USB_DEVICE_ATTACHED){
+                startUsb()
+            }else if(intent.action == UsbManager.ACTION_USB_DEVICE_DETACHED){
+                disconnect()
+            }
+        }
+    }
+
+    //var changing funcs connected to buttons
     fun bottom(view: android.view.View) {
         if(inTele){
             bottomT++
@@ -123,11 +223,11 @@ class MainActivity : AppCompatActivity() {
         var switch = findViewById<Switch>(R.id.switch2)
         switch.isChecked = true
 
-        var bar = findViewById<SeekBar>(R.id.slider_dSLASHo)
-        bar.progress = 50
+        //var bar = findViewById<SeekBar>(R.id.slider_dSLASHo)
+        //bar.progress = 50
 
         var inTele = false
-
+/*
         var check1 = findViewById<CheckBox>(R.id.checkBox)
         var check2 = findViewById<CheckBox>(R.id.checkBox2)
         var check3 = findViewById<CheckBox>(R.id.checkBox3)
@@ -137,15 +237,7 @@ class MainActivity : AppCompatActivity() {
         check2.isChecked = false
         check3.isChecked = false
         check4.isChecked = false
-/*
-no work
-
-        var teamNum = findViewById<TextInputLayout>(R.id.teamNum)
-        teamNum.editText to ""
-
-        var roundNum = findViewById<TextInputLayout>(R.id.roundNum)
-        roundNum.editText to " "
-*/
+        */
         var missText = findViewById<TextView>(R.id.text_missed)
         missText.text = "missed shots: " + missA.toString() + ":" + missT.toString();
 
@@ -157,6 +249,18 @@ no work
 
         var bottomText = findViewById<TextView>(R.id.text_bottom)
         bottomText.text = "bottom points: " + bottomA.toString() + ":" + bottomT.toString();
+    }
+    fun connect(view: android.view.View) {
+        startUsb()
+    }
+    //disconects serial lol
+    fun unCon(view: android.view.View) {
+        disconnect()
+    }
+
+    fun push(view: android.view.View) {
+        sendData(outerA.toString() + "-" + outerT.toString() + '-' + innerA.toString() + "-" + innerT.toString() + '-' + bottomA.toString() + "-" + bottomT.toString() + '-' + missA.toString() + "-" + missT.toString())
+
     }
 
 
